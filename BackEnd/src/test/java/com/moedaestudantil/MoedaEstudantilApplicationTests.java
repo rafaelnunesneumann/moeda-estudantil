a0be9c83da7d97742aec3d808aac78c32ce1c453
rafaelnunesneumann <rafaelnunesneumann123@gmail.com>
2026-05-07 11:50:23 -0300

    Criação back-end spring

---
+++ BackEnd/src/test/java/com/moedaestudantil/MoedaEstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moedaestudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoedaEstudantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
